0e23d3fa84c801c30978bacb6432c8a3a6852583
Ajay <ajaymungur@hotmail.com>
2018-11-26 17:15:02 +0000

    initialised jasmine, test drobe divisible by 3 and 5 methods

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,32 @@
+describe('FizzBuzz', function(){
+
+  var fizzbuzz
+
+  describe('knows when a number is', function(){
+    it('divisible by 3', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function(){
+    it('divisible by 3', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function(){
+    it('divisible by 5', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function(){
+    it('divisible by 5', function(){
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+    });
+  });
+});
